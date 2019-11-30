b8ea63688045a187e13571b21433a87370755a3f
hrufon <33417744+hrufon@users.noreply.github.com>
2019-12-01 00:34:33 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Pokemon/>
+  </div>
+</template>
+
+<script>
+
+import Pokemon from "./components/Pokemon";
+
+export default {
+  components: {
+      Pokemon,
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+  body,html{
+    background-image: url('https://wallpaperaccess.com/full/20997.jpg');
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+  display: grid;
+  width: 200vh;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+</style>
